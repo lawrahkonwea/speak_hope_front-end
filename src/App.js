c51eb45fbb3e4d44293fab8765369f8811e1c083
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-27 18:04:00 +0100

    import bootsrap tp app and start styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <>
-      <h1>mewwwww</h1>
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
